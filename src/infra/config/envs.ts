55d17f3d6ecb2c59f4c10d0054c3a9db62d6ad9b
wellhenrique <welissonh80@gmail.com>
2023-05-10 09:27:03 -0300

    feat: controller was created for a new session and envs were defined in a file

---
+++ src/infra/config/envs.ts
@@ -0,0 +1,5 @@
+
+export default {
+  securitySecretKey: process.env.SECURITY_SECRETKEY,
+  refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY
+}
--- src/infra/controllers/create-session.ts
+++ src/infra/controllers/create-session.ts
@@ -1,3 +1,4 @@
+import { GetTokenTypeUseCase } from "@/app/get-token-type";
 import { ValidateUser } from "@/contracts"
 import { CreateSession, RequestType } from "@/contracts/controller"
 import { CreateNewSession } from "@/contracts/usecase";
@@ -6,18 +7,23 @@ export class CreateSessionController implements CreateSession {
   constructor(
     private readonly validateUserUseCase: ValidateUser,
     private readonly createNewSessionUseCase: CreateNewSession,
+    private readonly getTokenTypeUseCase: GetTokenTypeUseCase,
   ) {}
 
   async handle(params: RequestType): Promise<string> {
     try {
-      const { email, password, tokenType } = params.request.body
-
+      const { email, password, neverExpire } = params.request.body
       const user = await this.validateUserUseCase.exec({
         email,
         password
       })
 
-      const { accessToken, token } = await this.createNewSessionUseCase.exec({user, tokenType});
+      const tokenType = await this.getTokenTypeUseCase.exec({
+        ...user,
+        neverExpire: !!neverExpire
+      });
+
+      const { accessToken, token } = await this.createNewSessionUseCase.exec({ user, tokenType });
       return accessToken;
     } catch (error) {
       return ''
