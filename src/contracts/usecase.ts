8cd9ecf718f53ab03762040c400c20b11573468f
wellhenrique <welissonh80@gmail.com>
2023-05-10 09:24:43 -0300

    chore: create CreateNewSessionOutput interface by crontracts

--- src/contracts/usecase.ts
+++ src/contracts/usecase.ts
@@ -1,7 +1,29 @@
-import { AuthenticateUserDTO } from "@/domain/usecases/authenticate-user.dto";
+import { AuthenticateUserDTO } from "@/domain/usecases/authenticate-user";
+import { User } from "@/domain/user";
 
 export interface UseCase<Input, Output> {
   exec(params?: Input): Output
 }
 
-export type AuthenticateUser = UseCase<AuthenticateUserDTO, Promise<void>>;
+export type ValidateUser = UseCase<AuthenticateUserDTO, Promise<User>>;
+
+export type generateTokenTypes = 'user' | 'driver' | 'company' | 'shipperInvite' | 'never-expire' | 'integration' | 'refreshToken'
+
+export interface CreateNewSessionInput {
+  user: User;
+  tokenType: generateTokenTypes
+}
+
+export interface CreateNewSessionOutput {
+  accessToken: string;
+  token: string;
+}
+
+export type CreateNewSession = UseCase<CreateNewSessionInput, Promise<CreateNewSessionOutput>>
+
+export type GetTokenTypeUserInput = User & {
+  neverExpire?: boolean;
+  cnpj?: string;
+
+}
+export type GetTokenType = UseCase<GetTokenTypeUserInput, Promise<generateTokenTypes>>
